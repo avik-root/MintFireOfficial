c16401774bc1c8ec849e9defcdee5b11951ac433
Avik Samanta <2025.aviksamanta@gmail.com>
2025-06-09 19:57:51 +0000

    Error: Invalid src prop (https://www.cnet.com/tech/services-and-software

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.cnet.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

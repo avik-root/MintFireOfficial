88ebbd8d673ee5402b87444b193eaa9efb2de0c2
Avik Samanta <2025.aviksamanta@gmail.com>
2025-06-11 20:22:39 +0000

    remove the iframe XSS and CSP features

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 
 import type {NextConfig} from 'next';
 
-const contentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
-  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' https://placehold.co https://avatars.githubusercontent.com https://learn.microsoft.com data:;
-  font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
-  connect-src 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  object-src 'none';
-  base-uri 'self';
-`;
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   /* config options here */
@@ -51,20 +38,8 @@ const nextConfig: NextConfig = {
         source: '/:path*',
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-          },
-          {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
           },
           {
             key: 'Referrer-Policy',
